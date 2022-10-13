f71ca45b115ebf90fafae1dac451cde29ee568cc
Tarık Kılıç Alsancak <95878587+TarikKilic248@users.noreply.github.com>
2022-10-13 18:22:23 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+using namespace std;
+
+class sekerMakinesi {
+public:
+  int toplamSeker = 10, alabilecekSeker, paraUstu;
+  int paraKontrol(int);
+  void sekerVer(int);
+};
+
+int sekerMakinesi::paraKontrol(int para) {
+  alabilecekSeker = para / 2;
+  if (alabilecekSeker < 1) {
+    cout << "Yetersiz para." << endl;
+  } else {
+    return alabilecekSeker;
+  }
+}
+
+void sekerMakinesi::sekerVer(int alabilecekSeker) {
+  if (toplamSeker == 0) {
+    cout << "Makinede hiç şeker yok." << endl;
+    cout << alabilecekSeker * 2 << " TL iade edildi." << endl;
+  }
+  if (alabilecekSeker > toplamSeker) {
+    paraUstu = (alabilecekSeker - toplamSeker) * 2;
+    cout << toplamSeker << " tane şeker aldınız." << endl;
+    cout << "Makinede daha fazla şeker kalmadığı için " << paraUstu
+         << " TL iade edildi" << endl;
+  } else {
+    paraUstu = toplamSeker - alabilecekSeker;
+    cout << alabilecekSeker << " tane şeker aldınız" << endl;
+    cout << "Makinede toplam " << paraUstu << " tane şeker kaldı.";
+  }
+}
+
+int main() {
+  int para;
+  cout << "***1 şeker 2tl***" << endl;
+  cout << "Makinada 10 şeker var" << endl;
+  cout << "Ne kadar para atıcaksınız ?" << endl;
+  cin >> para;
+
+  sekerMakinesi makine1;
+  int alabilecekSeker = makine1.paraKontrol(para);
+  makine1.sekerVer(alabilecekSeker);
+}
